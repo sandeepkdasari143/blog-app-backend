e6d33416eaeede3af8695e8ce4c4b2f77cf5d996
Sandeep K. Dasari <67505311+sandeepkdasari143@users.noreply.github.com>
2023-05-21 05:47:36 +0000

    Create, Update and Delete Blog Controller (100%)

--- src/blog.controller.js
+++ src/blog.controller.js
@@ -27,6 +27,7 @@ export const addBlog = bigPromise(async (req, res) => {
     }
 
     const blog = await Blog.create({
+        _id: blogID,
         blogTitle,
         blogSubTitle,
         authors,
@@ -36,4 +37,69 @@ export const addBlog = bigPromise(async (req, res) => {
             secure_url: cloudinaryResult?.secure_url
         }
     })
+
+    if (!blog) {
+        throw new Error("Blog is failed to be created at Database :(")
+    }
+    res.status(200).json({
+        success: true,
+        blog,
+    })
+
+});
+
+export const deleteBlog = bigPromise(async (req, res) => {
+    const {id: blogID} = req.params;
+
+    const blogToDelete = await Product.findById(blogID);
+
+    if(!blogToDelete){
+        throw new Error("Blog Not Found!")
+    }
+    
+    //Destroy the Existing Banner Image in the Cloudinary
+    await cloudinary.v2.uploader.destroy(blogToDelete.bannerImage.id);
+
+    await blogToDelete.remove();
+
+    res.status(200).json({
+        success: true,
+        message: "Blog has been deleted successfully",
+    });
+})
+
+export const updateBlog = bigPromise( async (req, res) => {
+    const {id: blogID} = req.params;
+
+    const blogToUpdate = await Product.findById(blogID);
+
+    if(!blogToUpdate){
+        throw new Error("Blog Not Found!")
+    }
+
+    let cloudinaryResult;
+
+    if(req.files){
+        //Destroy the Existing Banner Image in the Cloudinary
+        await cloudinary.v2.uploader.destroy(blogToDelete.bannerImage.id);
+        //Upload and Save the New Photos
+        let bannerImage = req.files.photo;
+        console.log(bannerImage)
+        cloudinaryResult = await cloudinary.v2.uploader.upload(bannerImage, {
+            folder: `${authors[0].authorID}/${blogID}`
+        });
+        console.log(cloudinaryResult);
+    }
+
+    blogToUpdate = await Blog.findByIdAndUpdate(blogID, req.body, {
+        new: true,
+        runValidators: true,
+        useFindAndModify: false
+    });
+
+    res.status(200).json({
+        success: true,
+        blogToUpdate
+    })
+    
 })
